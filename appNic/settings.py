27705d0ab7faf69396e1bf4d43e3c962c82b14ca
Joao Renato Vaz <joaorenatovaz@gmail.com>
2022-04-26 22:43:50 -0300

    Atualizando

--- appNic/settings.py
+++ appNic/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'appNic.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'base_templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
