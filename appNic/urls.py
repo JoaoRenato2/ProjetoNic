486c48d8a3e4159d4fa3387cfe05b4210ee1263a
Joao Renato Vaz <joaorenatovaz@gmail.com>
2022-05-01 17:26:03 -0300

    Atualizando

--- appNic/urls.py
+++ appNic/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from schedules.views import home
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('schedules.urls'))
+    path('', home),
+    path('auth/', include('schedules.urls'))
 ]
