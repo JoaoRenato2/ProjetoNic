2725dc95dab6f209b047ca90a6cacb7da566d9b5
JoaoRenato <joaorenatovaz@gmail.com>
2022-05-18 21:44:18 -0300

    Atualizando o form de agendamento

--- schedules/static/schedules/js/index.js
+++ schedules/static/schedules/js/index.js
@@ -1,7 +1,9 @@
 let btn = document.querySelector("#btn");
 let sidebar = document.querySelector(".sidebar");
 let searchBtn = document.querySelector(".bx-search");
-
+let addnew = document.querySelector("#schedule");
+let agend = document.querySelector(".panel-scheudle");
+let close_form = document.querySelector("#close-form");
 
 btn.onclick = function () {
     sidebar.classList.toggle("active");
@@ -11,3 +13,11 @@ searchBtn.onclick = function () {
     sidebar.classList.toggle("active");
 }
 
+
+addnew.onclick = function(){
+    agend.classList.add("active");
+}
+
+close_form.onclick = function(){
+    agend.classList.remove("active");
+}
